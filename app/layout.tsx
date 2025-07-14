0aa7f88d866dcee595044afa3922895e9ec37291
Shakib Khan <leadwithshakib@gmail.com>
2025-07-14 15:08:22 +0600

    RELEASE 1.0

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+export const metadata: Metadata = {
+  title: "MCCRSG",
+  description: "MCCRSG",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+        {children}
+      </ThemeProvider>
+      </body>
+    </html>
+  );
+}
